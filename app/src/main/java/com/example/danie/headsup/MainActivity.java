a0761015f91f2e183e0bfcc56a79218e82aca165
Skywalker45 <danielskywalker45@yahoo.com>
2017-03-20 09:02:53 -0600

    This is Daniel Salami, AKA Donfidididondada II.

--- app/src/main/java/com/example/danie/headsup/MainActivity.java
+++ app/src/main/java/com/example/danie/headsup/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.danie.headsup;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,6 +11,28 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final Button button = (Button) findViewById(R.id.button1);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick (View v) {
+                //load new intent here
+            }
+        });
+
+        final Button button2 = (Button) findViewById(R.id.button2);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick (View v) {
+                //load new intent here
+            }
+        });
+
+        final Button button3 = (Button) findViewById(R.id.button3);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick (View v) {
+                //load new intent here
+            }
+        });
+
     }
 
 }
