934859aa320a5e2fff3e540bf698002fbddc8e61
Skywalker45 <danielskywalker45@yahoo.com>
2017-03-31 08:37:39 -0600

    This is Daniel Salami, AKA Donfidididondada II.

--- app/src/main/java/com/example/danie/headsup/MainActivity.java
+++ app/src/main/java/com/example/danie/headsup/MainActivity.java
@@ -1,37 +1,65 @@
 package com.example.danie.headsup;
 
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+ //   Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.i("OnCreate", "OnCreate method has been executed");
 
-        final Button button = (Button) findViewById(R.id.button1);
+        final Button  button = (Button) findViewById(R.id.button1);
         button.setOnClickListener(new View.OnClickListener() {
             public void onClick (View v) {
                 //load new intent here
+                askquestion();
             }
         });
 
         final Button button2 = (Button) findViewById(R.id.button2);
-        button.setOnClickListener(new View.OnClickListener() {
+        button2.setOnClickListener(new View.OnClickListener() {
             public void onClick (View v) {
                 //load new intent here
             }
         });
 
         final Button button3 = (Button) findViewById(R.id.button3);
-        button.setOnClickListener(new View.OnClickListener() {
+        button3.setOnClickListener(new View.OnClickListener() {
             public void onClick (View v) {
                 //load new intent here
             }
         });
+
+    }
+
+
+    public void askquestion() {
+        AlertDialog.Builder alert = new AlertDialog.Builder(this);
+        alert.setTitle("Where is your syllabus?");
+        // alert.setMessage("Message");
+
+        alert.setPositiveButton("Take a picture", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int whichButton) {
+                //Your action here
+            }
+        });
+
+        alert.setNegativeButton("Choose folder",
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int whichButton) {
+                    }
+                });
+
+        alert.show();
 
     }
 
